d6d3de8b2c6e94879f2ce494a403dbd9808d07c3
Samir Gossain <samir.gossain@gmail.com>
2018-04-29 20:24:45 +0100

    Test working

--- test.py
+++ test.py
@@ -1,5 +1,23 @@
-import giphypop
-g = giphypop.Giphy()
-print(g)
-results = [x for x in g.search('kitten')]
-print(results[0])
+# Chose not to go with this package as it was fiddly and made more sense to  # #
+# lessen dependencies and use the API itself.  # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# import giphypop
+# g = giphypop.Giphy()
+# print(g)
+# results = [x for x in g.search('kitten')]
+# print(results[0])
+
+import json
+import requests
+
+YOUR_API_KEY = "02KjSiPr9Ur2Hizm8HsEdgB0NXPJiNBh"
+
+# data = json.loads(urllib.urlopen("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=" + YOUR_API_KEY + "&limit=5").read())
+# print json.dumps(data, sort_keys=True, indent=4)
+
+r = requests.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=" + YOUR_API_KEY + "&limit=5")
+# r.status_code
+# r.headers['content-type']
+# r.encoding
+print(r.text)
+print(r.json())
