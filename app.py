3fa83fc479fa6a0133e2983e094c7980bb9673a4
Samir Gossain <samir.gossain@gmail.com>
2018-04-29 15:24:18 +0100

    Adds a couple routes and form, form submissions routes to results page. results page needs to carry passed params

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
 ##
 # Index/Home Route
 ##################
-@app.route('/index')
+@app.route('/giflooper', methods=['GET'])
 @app.route('/')
 def index():
-    return 'Search Page'
+    return render_template('home.html')
 
 ##
 # Results Route
 ###############
-@app.route('/resultloop')
-def hello():
-    return 'Resultloop'
+@app.route('/resultloop', methods=['POST'])
+def resultloop():
+    # return 'Resultloop'
+    # searchterms = request.form['searchterms']
+    return render_template('resultloop.html')
+
+
+
+# @app.route('/hello/')
+# @app.route('/hello/<name>')
+# def hello(name=None):
+#     return render_template('hello.html', name=name)
