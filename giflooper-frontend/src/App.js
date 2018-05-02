83f4052e74f444f6e3b06b4b6c33ef3eae49e70d
Samir Gossain <samir.gossain@gmail.com>
2018-05-02 22:06:38 +0100

    Basic styling for React App

--- giflooper-frontend/src/App.js
+++ giflooper-frontend/src/App.js
@@ -8,11 +8,19 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">GifLooper</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="screenFiller">
+          <p className="App-intro">
+            <form>
+              <label>
+                searchterms:
+                <input type="text" name="name" />
+              </label>
+              <input type="submit" value="Submit" />
+            </form>
+          </p>
+        </div>
       </div>
     );
   }
